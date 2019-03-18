407b9999a81fcc8184e44556f67fbf10767e5d53
clever-puppet-5071 <yoonmi.kim@rutgers.edu>
2019-03-18 23:15:57 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,7 +3,7 @@
 katz_deli = []
 
 def line(arr)
-  puts "The line is currently empty." if arr.size == 0
+  puts "The line is currently empty." if arr.length == 0
   
   arr.each_with_index(1) { |el, i|
     puts "The line is currently: #{i}. #{el}"
@@ -18,7 +18,7 @@ def take_a_number(arr, str)
 end
 
 def now_serving(arr)
-  puts "There is nobody waiting to be served!" if (arr.size == 0)
+  puts "There is nobody waiting to be served!" if (arr.length == 0)
   puts "Currently serving " + arr.shift + "."
 end
 
